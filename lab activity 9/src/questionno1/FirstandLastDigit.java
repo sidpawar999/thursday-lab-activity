3451504156b68886f45807e046cfc7ae312c029a
Siddhesh <Siddhesh@DESKTOP-DKIIJ0F>
2023-01-20 19:09:52 +0530

    9th commit

---
+++ lab activity 9/src/questionno1/FirstandLastDigit.java
@@ -0,0 +1,22 @@
+package questionno1;
+
+public class FirstandLastDigit {
+
+	public static void main(String[] args) {
+		int number=502356997;
+	    int firstDigit = 0;
+	    int lastDigit = 0;
+
+	    lastDigit = number%10;		//for last digit
+	    
+	    while (number != 0) // To find the first digit of the number
+	    {
+	      firstDigit=number % 10;		
+	      number /= 10;
+	    }
+	      
+	    System.out.println("The First digit of the given number is: " + firstDigit);
+	    System.out.println("The Last digit of the given number is: " + lastDigit);
+	}
+
+}
---
+++ lab activity 9/src/questionno4/Pyramid.java
@@ -0,0 +1,22 @@
+package questionno4;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+int i, j, row=6;   
+		
+		for(i=0; i<row; i++)   //outer loop for rows  
+		{   
+		
+		for(j=0; j<=i; j++)   //inner loop for columns  
+		{   
+		
+		System.out.print("* ");   //prints stars   
+		}   
+		
+		System.out.println();   
+		}   
+
+	}
+
+}
